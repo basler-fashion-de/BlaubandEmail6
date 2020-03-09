7aadea73c7ea17fa26a2224d62dab6915c2cef86
Andrey <a.dubinskiy@softteco.com>
2020-03-09 15:10:52 +0300

    done config in plugin

---
+++ src/Subscriber/MailHeaderFooterSubscriber.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+
+namespace Blauband\EmailBase\Subscriber;
+
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Write\Validation\PostWriteValidationEvent;
+use Shopware\Core\Framework\Uuid\Uuid;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class MailHeaderFooterSubscriber implements EventSubscriberInterface
+{
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $salesChannelRepository;
+
+    public function __construct(EntityRepositoryInterface $salesChannelRepository)
+    {
+        $this->salesChannelRepository = $salesChannelRepository;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'Shopware\Core\Framework\DataAbstractionLayer\Write\Validation\PostWriteValidationEvent' => 'onSave'
+        ];
+    }
+
+    public function onSave(PostWriteValidationEvent $event): void
+    {
+        foreach ($event->getCommands() as $command) {
+            if (
+                $command->getEntityExistence()->getEntityName() !== 'system_config' ||
+                $command->getPayload()['configuration_key'] !== 'EmailBase.config.emailHeaderFooter'
+            ) {
+                continue;
+            }
+
+            $payload = $command->getPayload();
+            $mailHeaderFooterId = json_decode($payload['configuration_value'], true)['_value'];
+            $salesChannelBytesId = $payload['sales_channel_id'];
+
+            if (null === $salesChannelBytesId) {
+                continue;
+            }
+
+            $this->salesChannelRepository->update(
+                [
+                    ['id' => Uuid::fromBytesToHex($salesChannelBytesId), 'mailHeaderFooterId' => $mailHeaderFooterId],
+                ],
+                Context::createDefaultContext()
+            );
+        }
+    }
+}
