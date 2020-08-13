51050d2a4dccab07b8f9504a57881c098599c25c
Dennis Ottenbacher <mail@doapp.de>
2020-08-13 16:47:04 +0200

    Change LongTextWithHtml to new AllowHtml

--- src/Core/Content/LoggedMail/LoggedMailDefinition.php
+++ src/Core/Content/LoggedMail/LoggedMailDefinition.php
@@ -7,6 +7,7 @@ use Shopware\Core\Checkout\Order\OrderDefinition;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityDefinition;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\CreatedAtField;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\FkField;
+use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\AllowHtml;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\PrimaryKey;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\Flag\Required;
 use Shopware\Core\Framework\DataAbstractionLayer\Field\IdField;
@@ -44,7 +45,7 @@ class LoggedMailDefinition extends EntityDefinition
                 (new StringField('from_mail', 'fromMail'))->addFlags(new Required()),
                 (new StringField('to_mail', 'toMail'))->addFlags(new Required()),
                 (new StringField('subject', 'subject'))->addFlags(new Required()),
-                (new LongTextWithHtmlField('body_html', 'bodyHtml'))->addFlags(new Required()),
+                (new LongTextField('body_html', 'bodyHtml'))->addFlags(new Required(), new AllowHtml()),
                 (new LongTextField('body_plain', 'bodyPlain'))->addFlags(new Required()),
                 (new ManyToOneAssociationField('order', 'order_id', OrderDefinition::class, 'id', false)),
                 (new ManyToOneAssociationField('customer', 'customer_id', CustomerDefinition::class, 'id', false)),
